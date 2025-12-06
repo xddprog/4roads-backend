408182bbb7785ab9352ab94498d2a6280110048c
magomedov marif <magomedovmarif2@gmail.com>
2025-12-07 00:16:44 +0300

    fix image url

--- app/core/dto/category.py
+++ app/core/dto/category.py
@@ -1,7 +1,9 @@
 from datetime import datetime   
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+
+from app.utils.url_helper import get_absolute_url
 
 
 class CategoryModel(BaseModel):
@@ -12,3 +14,8 @@ class CategoryModel(BaseModel):
     image: str | None
     created_at: datetime
     updated_at: datetime
+
+    @field_validator("image")
+    @classmethod
+    def validate_image(cls, value: str | None) -> str | None:
+        return get_absolute_url(value)
